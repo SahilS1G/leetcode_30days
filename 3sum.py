598e0802baf2d68289b2a7903acfb3a18290227b
Sahil <sahimake1773@gmail.com>
2023-02-18 15:11:17 +0530

    threeSum

---
+++ 3sum.py
@@ -0,0 +1,67 @@
+'''
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+'''
+
+def threeSum():
+        # nums = [-1,0,1,2,-1,-4]
+        nums = [0,0,0,0]
+        # nums =[14,4,6,-1,10,9,-8,7,-13,14,-13,-11,-8,-9,11,14,-8,-14,-13,7,-10,-15,-13,-11,-11,11,14,13,2,-14,1,-7,-2,14,-1,-15,9,7,-1,3,6,1,7,5,-1,-5,4,-2,-4,-1,-9,-7,-1,-7,-11,3,12,10,-7,-1,12,1,8,-13,1,14,9,-13,6,-7,-3,-11,2,-11,10,-14,-1,-9,0,2,5,6,3,-11,6,7,0,3,3,0,-12,-8,-13,3,-14,-5,2,10,-11,-14,-12,1,-10,5,5,7,-1,11,14,6,-10,-4,-3,8,-7,10,1,8,-1,-11,-15,-6,-12,-13,12,-11]
+        arr = []
+        nums.sort()
+        count = 0
+        arr2 = []
+        for i in range(len(nums)):
+            if i > 0:
+                if nums[i] == nums[i-1]:
+                    continue
+            
+            l = i + 1
+            r = len(nums) - 1
+            
+            while l<r:
+                sum3 = nums[i] + nums[l] + nums[r]
+                if sum3>0:
+                    r -= 1
+                
+                elif sum3 < 0:
+                    l += 1
+                    
+                else:
+                    arr.append([nums[i], nums[l],nums[r]])
+                    l = l + 1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+        
+        print(arr)
+    
+threeSum()
